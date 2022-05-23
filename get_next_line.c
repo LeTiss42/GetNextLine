9dabc1d6ae6bfb1dd6af9bfa1f6b76355b734374
mravera <mravera@student.42lausanne.ch>
2022-05-23 18:00:17 +0200

    nettoyage

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mathis <mathis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/17 21:02:41 by mathis            #+#    #+#             */
-/*   Updated: 2022/05/19 03:00:08 by mathis           ###   ########.fr       */
+/*   Created: 2022/03/09 18:40:50 by mathis            #+#    #+#             */
+/*   Updated: 2022/05/17 21:00:11 by mathis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,25 @@
 char	*get_next_line(int fd)
 {
 	static char	*res;
-	char		buff[2];
-	size_t		j;
+	char		buff[BUFFER_SIZE + 1];
+	size_t		i;
 
-	buff[0] = 0;
-	buff[1] = 0;
-	if (fd < 0 || BUFFER_SIZE < 1 || (read(fd, 0, 0) != 0))
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0 || (read(fd, 0, 0) != 0))
 		return (NULL);
+	buff[BUFFER_SIZE] = 0;
 	if (!res)
 		res = gnl_calloc(1, sizeof(char));
-	j = gnl_strlen(res);
-	while (read(fd, buff, 1) > 0)
-	{
-		if (buff[0] == '\n')
-			return ((res = gnl_join(res, buff)) + j);
-		else
-			res = gnl_join(res, buff);
-	}
-	if (buff[0] == 0)
+	if (read (fd, buff, 1) == 0)
 		return (0);
-	return (res + j);
+	while (buff[i] != '\n' && buff[i] != 0 && i < BUFFER_SIZE - 1)
+		read (fd, buff + (++i), 1);
+	while (++i <= BUFFER_SIZE)
+		buff[i] = 0;
+	res = gnl_join(res, buff);
+	return (res);
 }
-/*
+
 #include <fcntl.h>
 
 int	main(void)
@@ -44,22 +41,20 @@ int	main(void)
 	int		fd;
 
 	fd = open("test.txt", O_RDONLY);
-	//printf("<<%s>>\n", get_next_line(fd));
-	//printf("<<%s>>\n", get_next_line(fd));
-	//printf("<<%s>>\n", get_next_line(fd));
-	//printf("<<%s>>\n", get_next_line(fd));
-	//printf("<<%s>>\n", get_next_line(fd));
-	//printf("<<%s>>\n", get_next_line(fd));
-	//printf("<<%s>>\n", get_next_line(fd));
-	
-	printf("||1 =>%s", get_next_line(fd));
-	printf("||2 =>%s", get_next_line(fd));
-	printf("||3 =>%s", get_next_line(fd));
-	printf("||4 =>%s", get_next_line(fd));
-	printf("||5 =>%s", get_next_line(fd));
-	printf("||6 =>%s", get_next_line(fd));
-	printf("||7 =>%s", get_next_line(fd));
-	printf("||8 =>%s", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
+	printf("<<%s>>\n", get_next_line(fd));
 	return (1);
 }
-*/
